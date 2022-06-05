24914ed191bd47281ad912258d1128fffdb5057d
sourabhmpandare <64161761+sourabhmpandare@users.noreply.github.com>
2022-06-05 21:25:34 +0530

    Add files via upload

---
+++ youtube.py
@@ -0,0 +1,22 @@
+import pywhatkit as kit
+# from selenium import webdriver
+# driver=webdriver.Chrome()
+# driver.get("https://www.youtube.com/")
+# inputserch=driver.find_element_by_xpath("/html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input")
+# inputserch.send_keys("Halamithi Habibo (Hindi) - Lyric Video | Beast | Thalapathy Vijay | Sun Pictures | Nelson | Anirudh0")
+# inputserchsymbol=driver.find_element_by_xpath("/html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/button/yt-icon")
+# inputserchsymbol.click()
+# inputsong=driver.find_element_by_xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[1]/div[1]/ytd-thumbnail/a/yt-img-shadow/img")
+# inputsong.play()
+
+
+# from selenium import webdriver
+# driver=webdriver.Chrome()
+# driver.get("https://www.youtube.com");
+# driver.findElement(By.id("search")).sendKeys("Happiness");
+# driver.findElement(By.id("search-icon-legacy")).click();
+
+
+kit.playonyt("Halamithi Habibo (Hindi) - Lyric Video | Beast | Thalapathy Vijay | Sun Pictures | Nelson | Anirudh0")
+
+print("PM song goal now next 15 days")
